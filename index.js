8696669db4f9e4b9a46a95289c3bda298de0291a
Wesley <wspencer3295@yahoo.com>
2021-02-23 12:18:47 -0700

    commit tree a1578a471fe5c2db0f985b8728bf7b75a6220d54

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+const random = Math.ceil(Math.random())
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+const maxArry = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+const max = Math.max(maxArry)
